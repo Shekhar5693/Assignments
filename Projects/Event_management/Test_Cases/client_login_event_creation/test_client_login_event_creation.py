be2e0671098973e2b1f66ceb5170d5b54a1a1df5
Shekhar Bahuguna <shekhu506@gmail.com>
2021-11-20 13:15:44 +0530

    Unfinished oop test

--- Projects/Event_management/Test_Cases/client_login_event_creation/test_client_login_event_creation.py
+++ Projects/Event_management/Test_Cases/client_login_event_creation/test_client_login_event_creation.py
@@ -1,20 +1,15 @@
 import pytest
-from webdriver_helpers import *
-
+import random
 from create_event import Test_login_event_creation
 
-@pytest.fixture
-def account(self):
-	account = SimpleNamespace()
-	account.username = "Lakshay"
-	account.password = "fakepassword1"
-	account.manager_account = "Mamooty"
-	account.wrong_username = "Lakshat"
-	account.wrong_password = "fakepassword"
-	account.number = random.randint(0,10)
-	account.budget = random.randint(5000,10000)
-	self.account = account
-	return account
+
+username = "Lakshay"
+password = "fakepassword1"
+manager_account = "Mamooty"
+wrong_username = "Lakshat"
+wrong_password = "fakepassword"
+number = random.randint(0,10)
+budget = random.randint(5000,10000)
 
 @pytest.fixture()
 def create_event(driver):
@@ -23,6 +18,6 @@ def create_event(driver):
 def test_login_wrong_username(create_event:Test_login_event_creation):
 	create_event.project_site()
 	create_event.client_login()
-	assert create_event.login_wrong_username(account.wrong_username, account.password) == "Invalid username."
+	assert create_event.login_wrong_username(wrong_username, password) == "Invalid username."
 
 	
